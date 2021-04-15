57eaf2db29d7b9778de4c189d2bffb2255d6bcf9
sondrerv <sondrervik@icloud.com>
2021-04-15 11:12:23 +0100

    Daemon RFID ferdig og funker

--- RFID/RFID_setup.sh
+++ RFID/RFID_setup.sh
@@ -4,6 +4,6 @@ sudo apt upgrade -y
 sudo apt install python3-dev python3-pip -y
 sudo pip3 install spidev
 sudo pip3 install mfrc522
+sudo cp read.py /usr/bin
 sudo cp read.service /lib/systemd/system
-sudo cp read.py /usr/lib
 sudo systemctl enable read.service
